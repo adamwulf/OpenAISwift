85e762c77de68c4e82d2e7c0b2e172bf189a2f7f
Adam Wulf <adam.wulf@gmail.com>
2023-02-02 22:15:16 -0600

    More integration tests

--- Tests/OpenAISwiftTests/OpenAISwiftTests.swift
+++ Tests/OpenAISwiftTests/OpenAISwiftTests.swift
@@ -3,20 +3,62 @@ import XCTest
 
 final class OpenAISwiftTests: XCTestCase {
 
-    static let Timeout: TimeInterval = 10
+    static let Timeout: TimeInterval = 15
     static let Token = "your key"
 
-    func testExample() throws {
+    func testConversation() throws {
         let openAI = OpenAISwift(authToken: Self.Token)
         let expectation = self.expectation(description: "expectation")
 
-        openAI.sendCompletion(with: "hello!") { result in
-            guard case .success = result else {
+        openAI.sendCompletion(with: "Please respond with 'hi'.\n\nResponse:") { result in
+            guard
+                case .success(let foo) = result,
+                let choice = foo.choices.first
+            else {
                 XCTFail()
                 return
             }
+            XCTAssert(choice.text.lowercased().contains("hi"))
             expectation.fulfill()
         }
         wait(for: [expectation], timeout: Self.Timeout)
     }
+
+    func testConversationStops() throws {
+        let openAI = OpenAISwift(authToken: Self.Token)
+        let expectation = self.expectation(description: "expectation")
+
+        openAI.sendCompletion(with: "Please respond with 'hi'.\n\nResponse:", stop: ["hi", "Hi"]) { result in
+            guard
+                case .success(let foo) = result,
+                let choice = foo.choices.first
+            else {
+                XCTFail()
+                return
+            }
+            XCTAssertFalse(choice.text.lowercased().contains("hi"))
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: Self.Timeout)
+    }
+
+    func testConversationSuffix() throws {
+        let openAI = OpenAISwift(authToken: Self.Token)
+        let expectation = self.expectation(description: "expectation")
+
+        openAI.sendCompletion(with: "When two people meet, they often say this two letter word:",
+                              suffix: "which is a very common greeting.") { result in
+            guard
+                case .success(let foo) = result,
+                let choice = foo.choices.first
+            else {
+                XCTFail()
+                return
+            }
+            XCTAssert(choice.text.lowercased().contains("hi"))
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: Self.Timeout)
+    }
+
 }
